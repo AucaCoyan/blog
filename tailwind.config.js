50f37080bfd795d40e4eef1a3daf2e82a0866871
Auca Maillot <aucacoyan@gmail.com>
2022-10-24 15:44:55 -0300

    :sparkles: add bio

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: { semiXL: "0 4px 12px 0 rgba(0,0,0,.12)" },
+      animation: {
+        "say-hi": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
