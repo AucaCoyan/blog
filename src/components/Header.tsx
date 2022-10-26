0e2a84666f322d92dbdc387de1410d8f9d566067
Auca Maillot <aucacoyan@gmail.com>
2022-10-26 19:03:48 -0300

    :children_crossing: better `Header.tsx`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,11 @@ import profile_pic from "../../utils/profile_pic.jpg";
 const Header = () => {
   return (
     <>
-      <nav className="bg-[#fffffe] dark:bg-[#121212bf] backdrop-saturate-180 py-3 sticky top-0 shadow-semiXL">
+      {/* Nav Bar (transparent, no titles or buttons) */}
+      <nav className="sticky top-0 z-10 py-3 mx-auto backdrop-saturate-180 backdrop-blur-lg shadow-semiXL bg-[121212bf]">
         <div className="flex items-center justify-between px-6 m-auto">
-          <Link href="/">
-            <>
+          <>
+            <Link href="/">
               {/* Logo */}
               <div className="flex items-center justify-between cursor-pointer">
                 <Image
@@ -19,25 +20,21 @@ const Header = () => {
                   alt="profile pic"
                   className="rounded-full"
                 />
-                <p className="ml-4">Auca Coyan</p>
+                <p className="ml-4 text-xl">Auca Coyan</p>
               </div>
+            </Link>
 
-              {/* Links */}
-
-              <ul className="flex pl-10">
-                {/* TODO: add a .map, for loop or cycle  */}
-                <li className="px-2 py-2">
-                  <Link href="/blog">Blog</Link>
-                </li>
-                <li className="px-2 py-2">
-                  <Link href="/">About</Link>
-                </li>
-                <li className="px-2 py-2">
-                  <Link href="/">Open Source</Link>
-                </li>
-              </ul>
-            </>
-          </Link>
+            {/* Left Links */}
+            <ul className="flex pl-10">
+              {/* TODO: add a .map, for loop or cycle  */}
+              <li className="px-2 py-2 ">
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li className="px-2 py-2">
+                <Link href="/">About</Link>
+              </li>
+            </ul>
+          </>
         </div>
       </nav>
     </>
