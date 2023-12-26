75bd5016ad2fd9dfc5ddd4009325316cd8c7a434
Auca Maillot <aucacoyan@gmail.com>
2023-12-26 17:19:36 -0300

    :rocket: add `vercel` adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import imageEndpoint from "astro-https-image-endpoint";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://aucacoyan.com",
-	integrations: [mdx(), sitemap(), tailwind()],
-	output: "server",
-	image: {
-		endpoint: imageEndpoint(import.meta.env.DEV),
-	},
+  site: "https://aucacoyan.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  image: {
+    endpoint: imageEndpoint(import.meta.env.DEV)
+  },
+  adapter: vercel()
 });
