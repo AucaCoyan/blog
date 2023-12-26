48cfdac8345a8d7daf4d2a4fbfcde7a988fba899
Auca Coyan <aucacoyan@gmail.com>
2023-12-26 17:24:14 -0300

    Revert ":sparkles: rework the post list and posts"
    This reverts commit f7dbc29be57737894bd6a6993c58381b28ab7226.

--- src/components/globalstyles.ts
+++ src/components/globalstyles.ts
@@ -1,3 +1,3 @@
-const global_styles = "min-h-screen bg-[#fffffe] text-black dark:bg-[#000] dark:text-white";
+const global_styles = "bg-[#fffffe] text-black dark:bg-[#000] dark:text-white";
 
 export default global_styles;
