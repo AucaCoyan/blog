ea9054a02fa5dc6d37617f2cf4104d65f9614b64
Auca Maillot <aucacoyan@gmail.com>
2023-12-26 17:01:19 -0300

    :sparkles: rework the post list and posts

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import imageEndpoint from 'astro-https-image-endpoint'
+import imageEndpoint from "astro-https-image-endpoint";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -8,10 +8,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://aucacoyan.com",
-    integrations: [mdx(), sitemap(), tailwind()],
-  image: {
-    endpoint: imageEndpoint(import.meta.env.DEV),
-  },
+	site: "https://aucacoyan.com",
+	integrations: [mdx(), sitemap(), tailwind()],
+	output: "server",
+	image: {
+		endpoint: imageEndpoint(import.meta.env.DEV),
+	},
 });
-
