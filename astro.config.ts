7ff16c5f09e6d52ccb2e52975bb643c69fd6e9a1
Auca Coyan <aucacoyan@gmail.com>
2023-12-26 17:24:14 -0300

    Revert ":rocket: add `vercel` adapter"
    This reverts commit f3305df01be716a810b4fca3160b45fee9cf722e.

--- astro.config.ts
+++ astro.config.ts
@@ -3,18 +3,15 @@ import sitemap from "@astrojs/sitemap";
 import imageEndpoint from "astro-https-image-endpoint";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://aucacoyan.com",
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  image: {
-    endpoint: imageEndpoint(import.meta.env.DEV)
-  },
-  adapter: vercel()
+	site: "https://aucacoyan.com",
+	integrations: [mdx(), sitemap(), tailwind()],
+	output: "server",
+	image: {
+		endpoint: imageEndpoint(import.meta.env.DEV),
+	},
 });
