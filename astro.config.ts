488c70ec5ee0374ef539a320edbf3448d75e8273
Auca Maillot <aucacoyan@gmail.com>
2025-10-31 08:01:38 -0300

    :adhesive_bandage: tiny fixes

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,15 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 
-// https://astro.build/config
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-    site: "https://aucacoyan.com",
-    integrations: [mdx(), sitemap()],
-    output: "static",
-    image: {
-        service: passthroughImageService(),
-    },
-    vite: {
-        plugins: [tailwindcss()]
-    }
+	site: "https://aucacoyan.com",
+	integrations: [mdx(), sitemap()],
+	output: "static",
+	image: {
+		service: passthroughImageService(),
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
